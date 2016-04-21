b944748cacf829164b7273ed573f6e6b6e222a4d
farmercode <wangchangchun120@gmail.com>
2016-04-21 23:57:59 +0800

    ssl配置文件

---
+++ RemoteProxy.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: king
+ * Date: 16/4/20
+ * Time: 上午1:56
+ */
+class RemoteProxy
+{
+    public $serverConfig = [
+        'ssl_cert_file' => __DIR__.'/ca/proxy.crt',
+        'ssl_key_file'  => __DIR__.'/ca/proxy.key'
+    ];
+}
